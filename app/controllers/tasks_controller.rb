cb32e046bc3acac2736d36c144734f0804718152
taro.kirameki <yokei.gomi@gmail.com>
2020-06-04 10:51:09 +0000

    change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
     
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show,:edit,:update,:destroy]
+    
     
     def index
         if logged_in?
@@ -55,4 +58,12 @@ class TasksController < ApplicationController
        params.require(:task).permit(:content, :status) 
     end
     
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @micropost
+            redirect_to root_url
+        end
+    end
+    
+  
 end
